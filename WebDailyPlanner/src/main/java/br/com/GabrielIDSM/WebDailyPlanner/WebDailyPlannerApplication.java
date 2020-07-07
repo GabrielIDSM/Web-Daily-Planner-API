99e651602b0d2acf405de02639367fb8e0f1f791
GabrielInacio <gabrielinacio@id.uff.br>
2020-07-07 13:12:53 -0300

    Projeto iniciado

---
+++ WebDailyPlanner/src/main/java/br/com/GabrielIDSM/WebDailyPlanner/WebDailyPlannerApplication.java
@@ -0,0 +1,13 @@
+package br.com.GabrielIDSM.WebDailyPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDailyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDailyPlannerApplication.class, args);
+	}
+
+}
