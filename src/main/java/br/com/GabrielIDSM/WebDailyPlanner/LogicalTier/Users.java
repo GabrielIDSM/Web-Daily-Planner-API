79260ee7ec00b65a241f68c02416014549726dcd
GabrielInacio <gabrielinacio@id.uff.br>
2020-09-02 12:14:19 -0300

    Test

--- src/main/java/br/com/GabrielIDSM/WebDailyPlanner/LogicalTier/Users.java
+++ src/main/java/br/com/GabrielIDSM/WebDailyPlanner/LogicalTier/Users.java
@@ -11,9 +11,9 @@ import java.util.Objects;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public abstract class Users {
-    
+
     private final static BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
-    
+
     public static boolean isUserWithEncoder(UserModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -27,10 +27,15 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUserWithEncoder(IdRequestModel user, List<UserModel> users) {
         try {
+            System.out.println("Email: " + user.getEmail());
+            System.out.println("Password: " + PASSWORD_ENCODER.encode(user.getPassword()));
+            System.out.println("---------------------------------------------------------");
             for (UserModel u : users) {
+                System.out.println("Email: " + u.getEmail());
+                System.out.println("Password: " + u.getPassword());
                 if (Objects.equals(u.getEmail(), user.getEmail())
                         && Objects.equals(u.getPassword(), PASSWORD_ENCODER.encode(user.getPassword()))) {
                     return true;
@@ -41,7 +46,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUserWithEncoder(GenericRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -55,7 +60,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUserWithEncoder(BirthdayRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -69,7 +74,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUserWithEncoder(DayRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -83,7 +88,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUserWithEncoder(EventIdRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -97,7 +102,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUser(IdRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -111,7 +116,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUser(DayRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -125,7 +130,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static boolean isUser(EventIdRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -139,7 +144,7 @@ public abstract class Users {
             return false;
         }
     }
-    
+
     public static UserModel getUser(IdRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -152,7 +157,7 @@ public abstract class Users {
             return null;
         }
     }
-    
+
     public static UserModel getUser(GenericRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -165,7 +170,7 @@ public abstract class Users {
             return null;
         }
     }
-    
+
     public static UserModel getUser(BirthdayRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -178,7 +183,7 @@ public abstract class Users {
             return null;
         }
     }
-    
+
     public static UserModel getUser(DayRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -191,7 +196,7 @@ public abstract class Users {
             return null;
         }
     }
-    
+
     public static UserModel getUser(EventIdRequestModel user, List<UserModel> users) {
         try {
             for (UserModel u : users) {
@@ -204,12 +209,12 @@ public abstract class Users {
             return null;
         }
     }
-    
-    public static UserModel newUser (UserModel user){
-        user.setPassword(PASSWORD_ENCODER.encode(user.getPassword()));
+
+    public static UserModel newUser(UserModel user) {
+        user.setPassword(PASSWORD_ENCODER.encode(user.getPassword()).replace("'", "''"));
         user.setName(user.getName().replace("'", "''"));
         user.setEmail(user.getEmail().replace("'", "''"));
         return user;
     }
-    
+
 }
